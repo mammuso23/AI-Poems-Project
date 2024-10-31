050b61d5efe87e761d1b4c43fb6d27c901ab114c
Mammuso Aliyah <mammuso@icloud.com>
2024-11-01 00:08:00 +0200

    User Experience update

--- crs/index.js
+++ crs/index.js
@@ -16,9 +16,9 @@ function generatePoem(event) {
     "You are a historic english major who loves to write short poems. You are required to generate an 8 line poem in basic HTML and separate each line. Just write the poem with no title. Do not include: ```html and ```. Make sure to follow the user instructions. Sign the poem at the end with 'SheCodes AI' inside a strong element. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛ Generating a poem about ${instructionsInput.value}...</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
